3d4c2c6c31416845311c33e16f9d1b7a3006473b
tkachenkos <serhi.tkachenko@gmail.com>
2020-06-17 16:48:17 +0300

    Generate new SSL keys for Nexus-host multiple IP

--- files/ssl_keys_generate.sh
+++ files/ssl_keys_generate.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 NEXUS_DOMAIN="nexus.example.com"
-NEXUS_IP_ADDRESS="192.168.56.121"
+NEXUS_IP_ADDRESS_1="192.168.56.121"
+NEXUS_IP_ADDRESS_2="192.168.77.96"
+NEXUS_IP_ADDRESS_3="192.168.17.47"
 SSL_PORT=8443
 keytool -genkeypair \
     -keystore keystore.jks \
@@ -12,5 +14,6 @@ keytool -genkeypair \
     -keysize 2048 \
     -validity 5000 \
     -dname "CN=*.${NEXUS_DOMAIN}, OU=Security, O=Example Corp., L=Kyiv, ST=Kyiv, C=UA" \
-    -ext "SAN=DNS:${NEXUS_DOMAIN},IP:${NEXUS_IP_ADDRESS}" -ext "BC=ca:true"
+    -ext "SAN=DNS:${NEXUS_DOMAIN},IP:${NEXUS_IP_ADDRESS_1},IP:${NEXUS_IP_ADDRESS_2},IP:${NEXUS_IP_ADDRESS_3}" \
+    -ext "BC=ca:true"
 # keytool -printcert -sslserver ${NEXUS_DOMAIN}:${SSL_PORT} -rfc
